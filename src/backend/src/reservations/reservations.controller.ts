053165685e082b75353233c2a52e5d83d55a388a
profejorgeemi-art <profejorgeemi@gmail.com>
2026-03-15 18:00:57 -0300

    Create ReservationsController for managing reservation endpoints

---
+++ src/backend/src/reservations/reservations.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { ReservationService } from './reservations.service';
+import { CreateReservationDto, UpdateReservationDto } from './dto';
+
+@Controller('reservations')
+export class ReservationsController {
+  constructor(private readonly reservationService: ReservationService) {}
+
+  @Post()
+  create(@Body() createReservationDto: CreateReservationDto) {
+    return this.reservationService.create(createReservationDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.reservationService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.reservationService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateReservationDto: UpdateReservationDto) {
+    return this.reservationService.update(id, updateReservationDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.reservationService.remove(id);
+  }
+}
