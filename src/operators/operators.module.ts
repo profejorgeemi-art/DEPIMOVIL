de961f311d2300bfba376240516e6507b88b2c66
profejorgeemi-art <profejorgeemi@gmail.com>
2026-03-15 21:43:08 -0300

    Create operators.module.ts for OperatorsModule implementation

---
+++ src/operators/operators.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { OperatorsService } from './operators.service';
+@Module({
+  providers: [OperatorsService],
+  exports: [OperatorsService],
+})
+export class OperatorsModule {}
